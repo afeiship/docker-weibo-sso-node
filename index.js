856f8807ae110edb74d2a4e60eca7da7f25605d4
afeiship <1290657123@qq.com>
2019-07-11 23:20:09 +0800

    add: pretieer

--- index.js
+++ index.js
@@ -7,22 +7,21 @@ const secretJson = require('./secret.json');
 const tokenJar = require('./.token.json');
 const MAX_CACHE_TIME = 3 * 86400000;
 
-
 //middleware:
 app.use(koaJson());
-app.use(async ctx => {
-    const updateTs = new Date(tokenJar.updated_at) * 1;
-    const nowTs = Date.now();
+app.use(async (ctx) => {
+  const updateTs = new Date(tokenJar.updated_at) * 1;
+  const nowTs = Date.now();
 
-    //> MAX_CACHE_TIME, update cache:
-    if ((nowTs - updateTs) > MAX_CACHE_TIME) {
-        const token = await weiboSso(secretJson);
-        tokenJar.updated_at = (new Date()).toISOString();
-        tokenJar.token = token;
-        fs.writeFileSync('./.token.json', JSON.stringify(tokenJar));
-    }
+  //> MAX_CACHE_TIME, update cache:
+  if (nowTs - updateTs > MAX_CACHE_TIME) {
+    const token = await weiboSso(secretJson);
+    tokenJar.updated_at = new Date().toISOString();
+    tokenJar.token = token;
+    fs.writeFileSync('./.token.json', JSON.stringify(tokenJar));
+  }
 
-    ctx.body = tokenJar;
+  ctx.body = tokenJar;
 });
 
 app.listen(3002);
